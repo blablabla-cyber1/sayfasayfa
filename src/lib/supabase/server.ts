705084da96b1e0dea955777967f3f8d865c0c8fa
blablabla-cyber1 <nadodblablabla@gmail.com>
2026-06-12 10:41:58 +0300

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,3 +1,12 @@
+export function setAll(cookiesToSet: CookieToSet[]) {
+  try {
+    cookiesToSet.forEach(({ name, value, options }) =>
+      cookieStore.set(name, value, options)
+    )
+  } catch (err) {
+    // ...
+  }
+}
 type CookieToSet = {
   name: string
   value: string
