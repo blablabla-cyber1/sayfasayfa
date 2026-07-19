aa060753e7dcfcfb18b7c54c14e073ec16ca047b
blablabla-cyber1 <nadodblablabla@gmail.com>
2026-07-19 17:56:37 +0300

    Update page.tsx

--- src/app/flashcards/page.tsx
+++ src/app/flashcards/page.tsx
@@ -1,5 +1,6 @@
-export const runtime = 'edge';
 'use client';
+export const runtime = 'edge';
+
 import { useEffect, useState, useCallback, useRef } from 'react';
 import Link from 'next/link';
 import {
